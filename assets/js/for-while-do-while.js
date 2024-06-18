32d9a3849c8bc8e5f1337473ab161d6161e35db1
hospingg <hospingg@gmail.com>
2024-06-18 11:02:02 +0300

    Initial commit

---
+++ assets/js/for-while-do-while.js
@@ -0,0 +1,45 @@
+// Кожне завдання вирішити циклами while, do..while, for.
+
+// 1. Вивести числа від 25 до 0 (порядок зменшення).
+
+for(let i = 25; i>=0; i--){
+    console.log(i)
+}
+// або
+
+// let count = 25
+// while(count>=0){
+//     console.log(count--)
+// } 
+
+
+
+// 2. Вивести числа від 10 до 50, які кратні 5.
+
+let count = 10
+while(count<=50){
+    console.log(count)
+    count+=5
+}
+// або
+
+// for(let i = 10; i<=50; i+=5){
+//         console.log(i)
+// }
+
+
+// 3. Знайти суму чисел в межах від 1 до 100.
+
+let sum = 0;
+let num = 1;
+while (num<=100){
+    sum+=num
+    num++
+}
+// або
+
+// for(let i = 1; i<=100; i++){
+//     sum+=i;
+// }
+
+console.log(sum)
